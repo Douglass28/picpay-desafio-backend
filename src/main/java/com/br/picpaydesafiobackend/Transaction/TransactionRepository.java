171bc9cc691ea9589d64f38736b3820a65286e8f
Douglass28 <douglas.souza1997@hotmail.com>
2024-04-12 15:42:08 -0300

    feat: edit name.

--- src/main/java/com/br/picpaydesafiobackend/Transaction/TransactionRepository.java
+++ src/main/java/com/br/picpaydesafiobackend/Transaction/TransactionRepository.java
@@ -2,6 +2,6 @@ package com.br.picpaydesafiobackend.Transaction;
 
 import org.springframework.data.repository.ListCrudRepository;
 
-public interface TransactionRepository extends ListCrudRepository<Transactions, Long> {
+public interface TransactionRepository extends ListCrudRepository<Transaction, Long> {
 
 }
